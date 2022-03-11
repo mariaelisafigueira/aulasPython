de359625641800923fa2f60995b81680566466a2
mariaelisafigueira <figueiramariaelisa@gmail.com>
2022-03-11 17:19:21 -0300

    Adivinhação com for e random

--- advinhacao_com_for.py
+++ advinhacao_com_for.py
@@ -1,28 +1,35 @@
+import random
+
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
+numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 3
-rodada = 1
 
-while(rodada <= total_de_tentativas):
+print(numero_secreto)
+
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute = int(input("Digite o seu número: "))
     print("Você digitou: ", chute)
-    
+
+    if(chute < 1 or chute > 100):
+        print ("Você deve digitar um número entre 1 e 100!")
+        continue
     acertou = chute ==  numero_secreto 
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
     if (acertou):
         print("Você acertou!")
+        break
     else:
         if(maior):    
                 print("Você errou! O seu chute foi maior que o numero secreto")
         elif(menor):  
                 print("Você errou! O seu chute foi menor que o numero secreto")
 
-    rodada = rodada + 1
 
 print("Fim do jogo")
