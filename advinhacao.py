2402f848fac89d3f0fea5db997c4db69d3136acd
mariaelisafigueira <figueiramariaelisa@gmail.com>
2022-03-11 11:53:09 -0300

    aulas de Python Alura

---
+++ advinhacao.py
@@ -0,0 +1,29 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
+
+while(rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = input("Digite o seu número: ")
+    print("Você digitou: ", chute)
+    chute = int(chute)
+
+    acertou = chute ==  numero_secreto 
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if(maior):    
+                print("Você errou! O seu chute foi maior que o numero secreto")
+        elif(menor):  
+                print("Você errou! O seu chute foi menor que o numero secreto")
+
+    rodada = rodada + 1
+
+print("Fim do jogo")
