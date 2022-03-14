5e1b831b2c90cd23ffa47682edc3d61126ebd59f
mariaelisafigueira <figueiramariaelisa@gmail.com>
2022-03-14 15:12:35 -0300

    finaçizando adivinhação

--- advinhacao_com_for.py
+++ advinhacao_com_for.py
@@ -6,7 +6,20 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+pontos = 1000
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 print(numero_secreto)
 
@@ -23,7 +36,7 @@ for rodada in range(1, total_de_tentativas + 1):
     menor = chute < numero_secreto
 
     if (acertou):
-        print("Você acertou!")
+        print("Você acertou e fez {} pontos!".format(pontos))
         break
     else:
         if(maior):    
@@ -31,5 +44,7 @@ for rodada in range(1, total_de_tentativas + 1):
         elif(menor):  
                 print("Você errou! O seu chute foi menor que o numero secreto")
 
+        pontos_perdidos = abs(numero_secreto - chute) 
+        pontos = pontos - pontos_perdidos
 
 print("Fim do jogo")
